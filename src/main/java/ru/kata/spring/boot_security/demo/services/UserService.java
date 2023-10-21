6278b663c0f77ecbb19a9a5ae8730a1200bc720d
Nikolay <jejel.n.o@mail.ru>
2023-10-21 17:26:40 +0300

    update task 3.1.3

---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserService.java
@@ -0,0 +1,19 @@
+package ru.kata.spring.boot_security.demo.services;
+
+import ru.kata.spring.boot_security.demo.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> showUsers();
+
+    User showById(long id);
+
+    void saveUser(User user);
+
+    void update(User user, Long id);
+
+    void delete(Long id);
+
+    User findByUsername(String username);
+}
