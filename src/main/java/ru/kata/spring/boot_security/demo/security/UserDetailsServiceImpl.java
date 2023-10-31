fc3d934792e549e9b6b5975a724a8ca04c2c53a9
Nikolay <jejel.n.o@mail.ru>
2023-10-31 22:21:49 +0300

    task 3.1.3 with problemsы

---
+++ src/main/java/ru/kata/spring/boot_security/demo/security/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package ru.kata.spring.boot_security.demo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.dao.UserDao;
+import ru.kata.spring.boot_security.demo.model.User;
+
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserDao userDao;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userDao.findByUserName(username);
+        user.orElseThrow(() -> new UsernameNotFoundException("User ot found"));
+        return user.get();
+    }
+}
